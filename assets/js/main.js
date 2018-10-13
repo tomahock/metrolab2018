cc80f3b71aea484abf8cd7af587aad160847bcc8
cdeath <github@nunopereira.net>
2018-10-13 19:03:39 +0100

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,16 @@
       map;
 
   function search() {
-    console.log(query);
+    console.log('query', query);
     if (!query.lat || !query.lng) return;
-    $(markers).each(function(idx, marker) {
+
+    markers.forEach(function(marker) {
       marker.setMap(null);
     });
     markers = [];
+
     $('#search-results').addClass('d-none').find('dl').html('');
+
     $.ajax({
       url: 'https://tomahock.com/cenas/amp/polygon.php',
       cors: true,
@@ -21,9 +24,9 @@
       dataType: 'json',
       data: JSON.stringify(query),
       success: function(res) {
-        //var bounds = new google.maps.LatLngBounds();
+        console.log('res', res);
         /* WHITELIST */
-        $(res.points).each(function(idx, point) {
+        res.points.forEach(function(point) {
           var location = {lat: point.loc.coordinates[0], lng: point.loc.coordinates[1]},
               marker = new google.maps.Marker({
                 position: location,
@@ -32,11 +35,9 @@
                 icon: '/assets/images/pin-whitelist.svg'
               });
           markers.push(marker);
-          console.log(marker);
-          //bounds.extend(marker);
         });
         /* BLACKLIST */
-        $(res.blackPoints).each(function(idx, point) {
+        res.blackPoints.forEach(function(point) {
           var location = {lat: point.loc.coordinates[0], lng: point.loc.coordinates[1]},
               marker = new google.maps.Marker({
                 position: location,
@@ -45,16 +46,14 @@
                 icon: '/assets/images/pin-blacklist.svg'
               });
           markers.push(marker);
-          console.log(marker);
-          //bounds.extend(location);
         });
-        /* FIT TO BOUNDS */
-        //map.fitBounds(bounds);
-        $('#search-results').removeClass('d-none').find('dl').html(Mustache.render(templates['search-results'], {
-          age: res.averageAge,
-          imi: res.imi,
-          weather: res.weather
-        }));
+        $('#search-results').removeClass('d-none').find('dl').html(
+          Mustache.render(templates['search-results'], {
+            age: res.averageAge,
+            imi: res.imi,
+            weather: res.weather
+          })
+        );
       },
       error: function() {
         console.log(arguments);
@@ -64,18 +63,13 @@
 
   function buildQuery() {
     var elms = $('input, select', $('#search-form')), white = [], black = [];
-    elms.each(function (idx, elm) {
-      elm = $(elm);
-      switch (true) {
-        case elm.is('[name^="search-condition"]'):
-          var condition = elm.val(),
-            value = elm.parent().find('[name^="search-rule"]').val();
-          if (!value) return;
-          if (condition === 'are') white.push(value);
-          if (condition === 'are not') black.push(value);
-          break;
-        default:
-          break;
+    elms.each(function() {
+      elm = $(this);
+      if (elm.is('[name^="search-condition"]')) {
+        var condition = elm.val(), value = elm.parent().find('[name^="search-rule"]').val();
+        if (!value) return;
+        condition === 'are' && white.push(value);
+        condition === 'are not' && black.push(value);
       }
     });
     $.extend(query, {white: white, black: black});
@@ -83,52 +77,36 @@
   }
 
   function initMap() {
-    var container = $('#map')[0], input = $('#search-where')[0];
+    var container = $('#map')[0],
+        input = $('#search-where')[0],
+        markers = [];
 
     map = new google.maps.Map(container, {
-      center: { lat: 41.154229, lng: -8.6193252 },
+      center: {lat: 41.154229, lng: -8.6193252},
       zoom: 13,
       mapTypeId: 'roadmap'
     }),
 
     where = new google.maps.places.SearchBox(input);
 
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
+    map.addListener('bounds_changed', function() {
       where.setBounds(map.getBounds());
     });
 
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    where.addListener('places_changed', function () {
+    where.addListener('places_changed', function() {
       var places = where.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      }
-
-      // Clear out the old markers.
+      if (places.length == 0) return;
       markers.forEach(function(marker) {
         marker.setMap(null);
       });
       markers = [];
-
-      // For each place, get the icon, name and location.
       var bounds = new google.maps.LatLngBounds();
       places.forEach(function (place) {
         if (!place.geometry) {
-          console.log("Returned place contains no geometry");
           return;
         } else {
           var b = place.geometry.viewport.toJSON();
-          $.extend(
-            query,
-            place.geometry.location.toJSON(),
-            {boundaries: [b.east, b.south, b.west, b.north].join(',')}
-          );
+          $.extend(query, place.geometry.location.toJSON(), {boundaries: [b.east, b.south, b.west, b.north].join(',')});
           search();
         }
         var icon = {
@@ -138,20 +116,15 @@
           anchor: new google.maps.Point(17, 34),
           scaledSize: new google.maps.Size(25, 25)
         };
-
-        // Create a marker for each place.
         markers.push(new google.maps.Marker({
           map: map,
           icon: icon,
           title: place.name,
           position: place.geometry.location
         }));
-
         if (place.geometry.viewport) {
-          // Only geocodes have viewport.
           bounds.union(place.geometry.viewport);
         } else {
-          console.log(place.geometry.location);
           bounds.extend(place.geometry.location);
         }
       });
@@ -160,9 +133,10 @@
   }
 
   /* SEARCH */
-  $('#search-form').on('click', '.add-remove-rule', function() {
-    var elm = $(this),
-        rules = $('[data-search-rule]').length;
+  $('#search-form')
+  .on('input', buildQuery)
+  .on('click', '.add-remove-rule', function() {
+    var elm = $(this), rules = $('[data-search-rule]').length;
     if (elm.is('.disabled')) {
       elm.prev('select').focus();
     } else if (elm.is('.remove')) {
@@ -172,17 +146,11 @@
       $('#search-form').append(Mustache.render(templates['search-rule'], {index: rules}));
       rules === 1 ? elm.remove() : elm.addClass('remove');
     }
-  });
-
-  $('#search-form').on('change', 'select', function() {
+  })
+  .on('change', 'select', function() {
     var elm = $(this);
     elm.next('.add-remove-rule').toggleClass('disabled', !elm.val());
   });
-
-  $('#search-form').on('input', function(evt) {
-    buildQuery();
-  });
-
 
   /* GOOGLE MAPS */
   $.ajax({
@@ -192,8 +160,8 @@
   });
 
   /* MUSTACHE */
-  $('script[type="x-tmpl-mustache"]').each(function (idx, elm) {
-    elm = $(elm);
+  $('script[type="x-tmpl-mustache"]').each(function() {
+    elm = $(this);
     template = elm.attr('data-template');
     templates[template] = elm.html();
     Mustache.parse(templates[template]);
